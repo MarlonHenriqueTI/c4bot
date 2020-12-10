7e90c4dd1f38c0a7999e3de25d5e8532b3163b50
Marlon Henrique <contato@marlonhenrique.com>
2020-12-10 10:51:10 -0300

    correções no código

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -1,34 +1,34 @@
-const processPostback = require ('../processos / postback'); 
-const processMessage = require ('../ processos / mensagens');
-module.exports = function (app, giz) { 
-  app.get ('/ webhook', function (req, res) { 
-    if (req.query ['hub.verify_token'] === process.env.VERIFY_TOKEN) { 
-       console .log ('webhook verificado'); 
-       res.status (200) .send (req.query ['hub.challenge']); 
-    } else { 
-        console.error ('verificação falhou. Token incompatível.'); 
-        res.sendStatus (403); 
-     } 
-  }); 
+const processPostback = require('../processes/postback');
+const processMessage = require('../processes/messages');
+module.exports = function(app, chalk){
+  app.get('/webhook', function(req, res) {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+       console.log('webhook verified');
+       res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error('verification failed. Token mismatch.');
+        res.sendStatus(403);
+     }
+  });
   
-  app.post ('/ webhook', function (req, res) { 
-    // verificar a assinatura da página.
-     if (req.body.object === 'page') { 
+  app.post('/webhook', function(req, res) {
+    //checking for page subscription.
+    if (req.body.object === 'page'){
        
-       / * Iterar sobre cada entrada, pode haver várias entradas 
-       se os retornos de chamada forem em lote. * /
-       req.body.entry.forEach (function (entry) { 
-       // Iterar sobre cada entrada de evento de
-           mensagem.messaging.forEach (function (event) { 
-          console.log (event); 
-          if (event.postback) { 
-             processPostback (event); 
-          } else if (event.message) { 
-             processMessage (event); 
-          } 
-      }); 
-    }); 
-    res.sendStatus (200); 
-   } 
-  }); 
+       /* Iterate over each entry, there can be multiple entries 
+       if callbacks are batched. */
+       req.body.entry.forEach(function(entry) {
+       // Iterate over each messaging event
+          entry.messaging.forEach(function(event) {
+          console.log(event);
+          if (event.postback){
+             processPostback(event);
+          } else if (event.message){
+             processMessage(event);
+          }
+      });
+    });
+    res.sendStatus(200);
+   }
+  });
 }
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require ('express') 
-const morgan = require ('morgan'); 
-const bodyParser = require ('body-parser');
-const app = express ();
-// configuração do aplicativo
- app.set ('port', (process.env.PORT || 3000));
-// configurar nosso aplicativo expresso
-app.use (morgan ('dev')); // registra todas as solicitações no console. 
-app.use (bodyParser.urlencoded ({extended: false})); 
-app.use (bodyParser.json ());
-// rotas do aplicativo
- require ('./ routes / webhook_verify') (app);
-// aquecendo os motores !! setta !! vai !!!.
-app.listen (app.get ('port'), function () { 
-  const url = 'http: // localhost:' + app.set ('port'); 
-  console.log ('Aplicativo em execução na porta:', app.get ('porta'));
+const express = require(‘express’)
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+// app configuration
+app.set('port', (process.env.PORT || 3000));
+// setup our express application
+app.use(morgan('dev')); // log every request to the console.
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json()); 
+// app routes
+require('./routes/webhook_verify')(app);
+// warming up the engines !! setta !! go !!!.
+app.listen(app.get('port'), function() {
+  const url = 'http://localhost:' + app.set('port');
+  console.log('Application running on port: ', app.get('port'));
 });
